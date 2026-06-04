78a8bbcffd6dc4155d12fe7564264a2a5277602d
JakubKlimaszewski93691 <jk93691@student.uwb.edu.pl>
2026-06-04 21:12:33 +0200

    Add destructor to Stack class for memory cleanup

--- Stack.h
+++ Stack.h
@@ -7,14 +7,39 @@
 #include "Node.h"
 
 template <typename T>
-class Stack{
+class Stack {
     Node<T> *topNode;
 public:
     Stack() : topNode(nullptr) {}
 
+    ~Stack() {
+        while (topNode != nullptr) {
+            pop();
+        }
+    }
+
+    void push(T value) {
+        Node<T>* newNode = new Node<T>(value);
+        newNode->setPrevNode(topNode);
+        topNode = newNode;
+    }
+
+    T pop() {
+        if (topNode == nullptr) {
+            std::cout << "Stos jest pusty" << std::endl;
+            return T();
+        }
+        Node<T>* nodeToRemove = topNode;
+        T valueToReturn = nodeToRemove->getValue();
+        topNode = topNode->getPrevNode();
+        delete nodeToRemove;
+        return valueToReturn;
+    }
+
     T peek() {
         if (topNode == nullptr) {
-            cout << "Stos jest pusty" << endl;
+            std::cout << "Stos jest pusty" << std::endl;
+            return T();
         }
         else {
             return topNode->getValue();
